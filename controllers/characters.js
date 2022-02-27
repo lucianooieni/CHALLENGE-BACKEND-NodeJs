11326efca7563820062e06964776ba55731615b0
lucianooieni21 <lucianooieni@mi.unc.edu.ar>
2022-02-27 00:25:30 -0300

    adding CRUD character (4)

--- controllers/characters.js
+++ controllers/characters.js
@@ -9,4 +9,52 @@ characterRouter.get('/', async (request, response) => {
   response.json(users)
 })
 
+characterRouter.post('/', async (request, response) => {
+  console.log('body: ', request.body)
+  try {
+    const { image, name, age, weight, history } = request.body
+    const user = await Character.create({ image, name, age, weight, history })
+    response.json(user)
+  } catch (error) {
+    response.json(error)
+  }
+})
+
+characterRouter.put('/:id', async (request, response) => {
+  try {
+    const { image, name, age, weight, history } = request.body
+    const updatedCharacter = await Character.update(
+      { image, name, age, weight, history },
+      {
+        where: {
+          id: request.params.id
+        }
+      }
+    )
+    console.log('updatedCharacter', updatedCharacter) // [ 1 ]
+    response.json(updatedCharacter)
+  } catch (error) {
+    response.json(error)
+  }
+})
+
+characterRouter.delete('/:id', async (request, response) => {
+  try {
+    const deletedCharacter = await Character.destroy({
+      where: {
+        id: request.params.id
+      }
+    })
+    response.json(deletedCharacter)
+  } catch (error) {
+    response.json(error)
+  }
+})
+
 module.exports = characterRouter
+
+// image: DataTypes.STRING,
+// name: DataTypes.STRING,
+// age: DataTypes.INTEGER,
+// weight: DataTypes.DOUBLE,
+// history: DataTypes.STRING
